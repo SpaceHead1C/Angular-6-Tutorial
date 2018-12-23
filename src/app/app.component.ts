bd210244c751837f354be5ba0c08342e21a0d80a
SpaceHead1C <damh17@gmail.com>
2018-12-23 16:35:07 +0300

    function for user's decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+function log(target, name, descriptor) {
+  console.log(target, name, descriptor);
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
