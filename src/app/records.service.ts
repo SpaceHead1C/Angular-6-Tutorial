5905287b2e567af5e81af1ba63ffa1b17d8479d5
SpaceHead1C <damh17@gmail.com>
2018-12-23 22:36:53 +0300

    service can return some data

--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -6,4 +6,33 @@ import { Injectable } from '@angular/core';
 export class RecordsService {
 
   constructor() { }
+
+  getData() {
+    return [
+      {
+        name: 'some',
+        online: false
+      },
+      {
+        name: 'this',
+        online: true
+      },
+      {
+        name: 'that',
+        online: true
+      },
+      {
+        name: 'other',
+        online: false
+      },
+      {
+        name: 'nobody',
+        online: true
+      },
+      {
+        name: 'spy',
+        online: false
+      }
+    ];
+  }
 }
